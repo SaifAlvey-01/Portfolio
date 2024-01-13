3276ac6db09d4ea50ead77686f3a13a2a1753b18
SAIF UR RAHMAN <sp20-bcs-088@isbstudent.comsats.edu.pk>
2024-01-13 20:01:51 +0500

    first commit

--- pages/api/getPageInfo.ts
+++ pages/api/getPageInfo.ts
@@ -15,7 +15,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
   ) {
-    const pageInfo: PageInfo[] = await sanityClient.fetch(query);
+    try{    const pageInfo: PageInfo[] = await sanityClient.fetch(query);
     res.status(200).json({ pageInfo })
-  }
-  
+  }catch(e){}
+}
